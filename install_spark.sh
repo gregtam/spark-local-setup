a624a060922fc97722f0e750b8b768255823c372
gregtam <gregtam1991@gmail.com>
2017-04-06 10:18:15 -0400

    Remove the source line in the bash script since it doesn't work. Add alias for sparkR.

--- install_spark.sh
+++ install_spark.sh
@@ -8,5 +8,4 @@ echo "# Setting up shortcuts for Spark" >> ~/.bash_profile
 echo "export SPARK_PATH=~/spark-2.1.0-bin-hadoop2.7" >> ~/.bash_profile
 echo "alias pyspark=\"$SPARK_PATH/bin/pyspark --master local[4]\"" >> ~/.bash_profile
 echo "alias snotebook=\"PYSPARK_DRIVER_PYTHON='jupyter' PYSPARK_DRIVER_PYTHON_OPTS='notebook' pyspark\"" >> ~/.bash_profile
-
-source ~/.bash_profile
+echo "alias sparkR=\"$SPARK_PATH/bin/sparkR --master local[4]\"" >> ~/.bash_profile
