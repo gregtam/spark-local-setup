0ec07f03c03998a43e5dcc91a2bb196d415fe213
gregtam <gregtam1991@gmail.com>
2017-05-30 10:47:06 -0400

    Change to use Spark 2.1.1

--- install_spark.sh
+++ install_spark.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+!/bin/bash
 cd ~
-wget http://d3kbcqa49mib13.cloudfront.net/spark-2.1.0-bin-hadoop2.7.tgz
-tar -xvf spark-2.1.0-bin-hadoop2.7.tgz
-rm spark-2.1.0-bin-hadoop2.7.tgz
+wget https://d3kbcqa49mib13.cloudfront.net/spark-2.1.1-bin-hadoop2.7.tgz
+tar -xvf spark-2.1.1-bin-hadoop2.7.tgz
+rm spark-2.1.1-bin-hadoop2.7.tgz
 
 echo "# Setting up shortcuts for Spark" >> ~/.bash_profile
-echo "export SPARK_PATH=~/spark-2.1.0-bin-hadoop2.7" >> ~/.bash_profile
-echo "alias pyspark=\"$SPARK_PATH/bin/pyspark --master local[4]\"" >> ~/.bash_profile
+echo "export SPARK_PATH=~/spark-2.1.1-bin-hadoop2.7" >> ~/.bash_profile
+echo "alias pyspark=\"~/spark-2.1.1-bin-hadoop2.7/bin/pyspark --master local[4]\"" >> ~/.bash_profile
 echo "alias snotebook=\"PYSPARK_DRIVER_PYTHON='jupyter' PYSPARK_DRIVER_PYTHON_OPTS='notebook' pyspark\"" >> ~/.bash_profile
-echo "alias sparkR=\"$SPARK_PATH/bin/sparkR --master local[4]\"" >> ~/.bash_profile
+echo "alias sparkR=\"~/spark-2.1.1-bin-hadoop2.7/bin/sparkR --master local[4]\"" >> ~/.bash_profile
